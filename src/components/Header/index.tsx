b8cc8e29573243181336bdcf5f5a8f42436dd40d
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-05-18 10:07:59 -0400

    :sparkles: created newTransactionsModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
-import logoImg from '../../assets/logo.svg'
 import { Button } from '../Button'
+import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
+import { IHeader } from './types'
 
-export function Header() {
+export function Header({ onOpenNewTransactionModal }: IHeader) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
 
-        <Button text="Nova Transação" />
+        <Button text="Nova Transação" onClick={onOpenNewTransactionModal} />
       </Content>
     </Container>
   )
