ce8b5856e1e4f9b5857b0d3a3520b07ba661398d
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-05-18 20:29:35 -0400

    :lipstick: modal content

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,8 @@
 import Modal from 'react-modal'
 import { Button } from '../Button'
 import { Input } from '../Input'
-import { Form } from './styles'
+import closeImg from '../../assets/close.svg'
+import { Content, Form } from './styles'
 import { INewTransactionModal } from './types'
 
 Modal.setAppElement('#root')
@@ -17,15 +18,27 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <Form>
-        <h2>Cadastrar transação</h2>
+      <Content>
+        <button
+          type="button"
+          className="react-modal-close"
+          onClick={onRequestClose}
+        >
+          <img src={closeImg} alt="Close modal" />
+        </button>
 
-        <Input placeholder="Título" />
-        <Input placeholder="Valor" />
-        <Input placeholder="Categoria" />
+        <Form>
+          <h2>Cadastrar transação</h2>
 
-        <Button secondaryStyle>Cadastrar</Button>
-      </Form>
+          <Input placeholder="Título" />
+          <Input placeholder="Valor" />
+          <Input placeholder="Categoria" />
+
+          <Button secondaryStyle type="submit">
+            Cadastrar
+          </Button>
+        </Form>
+      </Content>
     </Modal>
   )
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,12 +1,28 @@
 import styled from 'styled-components'
 
+export const Content = styled.div`
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background-color: transparent;
+
+    transition: all 0.2s ease;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+`
+
 export const Form = styled.form`
   h2 {
     color: var(--text-title);
     margin-bottom: 2rem;
   }
 
-  button {
+  button[type='submit'] {
     margin-top: 1.5rem;
   }
 `
